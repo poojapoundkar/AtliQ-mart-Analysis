ada8667bc951d46b59aae635e405586adc78c046
Pooja <poojapoundkar1995@gmail.com>
2024-03-05 15:53:28 +0530

    Update 3.sql

--- SQL Files/3.sql
+++ SQL Files/3.sql
@@ -4,29 +4,10 @@ campaign_name,
 totaI_revenue(before_promotion), 
 totaI_revenue(after_promotion). */
 
- WITH PromoPrices AS (
-    SELECT
-        fe.*,
-        CASE
-            WHEN fe.promo_type = '50% OFF' THEN fe.base_price * 0.5
-            WHEN fe.promo_type = '33% OFF' THEN fe.base_price * 0.67
-            WHEN fe.promo_type = '25% OFF' THEN fe.base_price * 0.75
-            WHEN fe.promo_type = '500 Cashback' THEN fe.base_price - 500
-            WHEN fe.promo_type = 'BOGOF' THEN fe.base_price / 2
-            ELSE fe.base_price  
-        END AS promo_price
-    FROM
-        fact_events fe
-)
-SELECT
-    dc.campaign_name,
-    ROUND(SUM(f.`quantity_sold(before_promo)` * f.base_price) / 1000000, 2) AS total_revenue_before_promotion,
-    ROUND(SUM(f.`quantity_sold(after_promo)` * p.promo_price) / 1000000, 2) AS total_revenue_after_promotion
-FROM
-    dim_campaigns dc
-JOIN
-    fact_events f ON dc.campaign_id = f.campaign_id
-JOIN
-    PromoPrices p ON p.event_id = f.event_id
-GROUP BY
-    dc.campaign_name;
+ select distinct(p1.campaign_name) as campaigns, 
+concat(round(sum(p2.`quantity_sold(before_promo)`*p2.base_price)/1000000,2),' ' 'M') as revenue_before_promo, CONCAT(round(sum(p2.`quantity_sold(after_promo)`*p2.base_price)/1000000,2),' ' 'M') as revenue_after_revenue
+from fact_events as p2
+inner join dim_campaigns as p1
+ON p1.campaign_id = p2.campaign_id
+group by campaigns
+order by campaigns asc;
